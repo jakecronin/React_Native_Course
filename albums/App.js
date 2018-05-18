ab789111000857e8d93773c56f27ac3a2abf4d01
jakecronin <jake.cronin4@gmail.com>
2018-05-18 01:00:41 -0400

    first commit

---
+++ albums/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+
+export default class App extends React.Component {
+  render() {
+    console.log('App rendering');
+    return (
+      <View style={styles.container}>
+        <Header headerText={'Albums!!!'}/>
+        <AlbumList/>
+      </View>
+    );
+  }
+}
+
+// Formatting for base of app
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+  },
+});
